dba40f8bcf29c919483de64463635aa77f2d4345
chukynya <henry.christian7557@gmail.com>
2025-02-06 10:25:05 +0700

    Final Result - 01

--- src/exercise_library_01/Borrower.java
+++ src/exercise_library_01/Borrower.java
@@ -11,16 +11,25 @@ public class Borrower {
 	}
 	
 	public void displayBorrowedBooks() {
-		System.out.println(name + " Borrowed Books: ");
+		System.out.println(name + "'s Borrowed Books:");
 		for(Book book: books) {
 			System.out.println("Title: " + book.getBookTitle());
 			System.out.println("ISBN: " + book.getBookID());
 			System.out.println("Author: " + book.getBookAuthor());
 			System.out.println("Biography: " + book.getBookBiography());
+			System.out.println("-----------------------------------");
 		}
 	}
 
 	public void addBooks(Book book) {
-		this.books.add(book);
+		books.add(book);
+	}
+	
+	public void removeBooks(Book book) {
+		books.remove(book);
+	}
+
+	public String getName() {
+		return name;
 	};
 }
--- src/exercise_library_01/Library.java
+++ src/exercise_library_01/Library.java
@@ -1,11 +1,16 @@
 package exercise_library_01;
 
-import java.util.HashMap;
+import java.util.HashSet;
 
 public class Library {
-	private HashMap<Book, Book> books;
+	private HashSet<Book> books;
+	
+	public Library() {
+		this.books = new HashSet<Book>();
+	}
+	
 	public boolean addBook(Book book) {
-		books.put(book, book);
+		books.add(book);
 		System.out.println("Book added to library: " + book.getBookTitle());
 		return true;
 	}
@@ -13,6 +18,35 @@ public class Library {
 	public boolean borrowBook(Borrower borrower, Book book) {
 		if(book.isAvailable()) {
 			borrower.addBooks(book);
+			book.setAvailable(false);
+			System.out.println(borrower.getName() + " borrowed: " + book.getBookTitle());
+			return true;
+		}
+		System.out.println("Can't borrow book");
+		return false;
+	}
+	
+	public boolean returnBook(Borrower borrower, Book book) {
+		if(!book.isAvailable()) {
+			borrower.removeBooks(book);
+			book.setAvailable(true);
+			System.out.println(borrower.getName() + " returned: " + book.getBookTitle());
+			return true;
+		}
+		System.out.println("Can't return book");
+		return false;
+	}
+	
+	public void displayLibraryBooks() {
+		System.out.println("Books in library:");
+		for(Book book: books) {
+			if(book.isAvailable()) {
+				System.out.println("Title: " + book.getBookTitle());
+				System.out.println("ISBN: " + book.getBookID());
+				System.out.println("Author: " + book.getBookAuthor());
+				System.out.println("Biography: " + book.getBookBiography());
+				System.out.println("---------------------------------");
+			}
 		}
 	}
 }
